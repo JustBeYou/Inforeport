e99ebef730091de9e162c88257392df58dc9f901
JustBeYou <mihailferaru2000@gmail.com>
2018-07-22 18:46:45 +0300

    Add method for creating a new connection to the default database

--- info_report/lib/InfoReport/Schema/Schema.pm
+++ info_report/lib/InfoReport/Schema/Schema.pm
@@ -6,3 +6,8 @@ use base qw/DBIx::Class::Schema/;
 # This will load any classes within
 # Moblo::Schema::Result and Moblo::Schema::ResultSet (if any)
 __PACKAGE__->load_namespaces();
+
+sub getConnection {
+    my $self = shift;
+    return $self->connection('dbi:SQLite:inforeport.db');
+}
