f0331d89be2df05690eedb50a223123aeae45974
JustBeYou <mihailferaru2000@gmail.com>
2018-07-23 17:09:53 +0300

    Implement reading from database and test entry creation

--- info_report/lib/InfoReport/Model/Users.pm
+++ info_report/lib/InfoReport/Model/Users.pm
@@ -1,2 +1,52 @@
+package InfoReport::Model::Users;
+use InfoReport::Schema;
+use InfoReport::Model::Scraper;
+
+my $schema = InfoReport::Schema->getConnection();
+my $resultset = $schema->resultset('Users');
+
+sub updateUserData {
+    my $username = shift;
+
+    my $data = readUserData($username);
+
+    if (defined $data) {
+        # if already exists, update
+    } else {
+        # else create entry
+        # TODO: implement this for real
+
+        $resultset->create({
+                id => 1,
+                username => 'mihai',
+                solved => 100,
+                tried => 10,
+                totalSubmissions => 500,
+                activityData => '{}',
+                lastSubmission => ''
+            });
+    }
+
+
+}
+
+sub readUserData {
+    my $username = shift;
+
+    my $query = $resultset->search({username => $username});
+    return $query->first;
+}
+
+sub checkIfUserChanged {
+    my $username = shift;
+    my $lastDate = shift;
+
+
+}
+
+sub generateActivityReport {
+    my $username = shift;
+    my $data     = shift;
+}
 
 1;
