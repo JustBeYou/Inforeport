c78fb1863383877d857a7f3530e0009987bb9afd
JustBeYou <mihailferaru2000@gmail.com>
2018-07-23 10:47:43 +0300

    Remove some code

--- info_report/lib/InfoReport/Model/Submission.pm
+++ info_report/lib/InfoReport/Model/Submission.pm
@@ -5,14 +5,5 @@ use warnings;
 
 sub new {
     my ($class, %args) = @_;
-    croak "bad arguments" unless 
-        defined $self->{id} and 
-        defined $self->{username} and
-        defined $self->{problem} and
-        defined $self->{competition} and
-        defined $self->{size} and
-        defined $self->{date} and
-        defined $self->{status} and
-        defined $self->{score};
     return bless { %args }, $class;
 }
