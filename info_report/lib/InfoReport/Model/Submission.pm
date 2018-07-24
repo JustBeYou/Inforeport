8410ca1da5b1ee6f89a034a9eb1007ac9247e9b0
JustBeYou <mihailferaru2000@gmail.com>
2018-07-24 13:04:48 +0300

    Add day,month,year attributes

--- info_report/lib/InfoReport/Model/Submission.pm
+++ info_report/lib/InfoReport/Model/Submission.pm
@@ -5,6 +5,13 @@ use warnings;
 
 sub new {
     my ($class, %args) = @_;
+
+    my $date = $args{date};
+    $date =~ m/(\d+) ([a-z]+) (\d+)/; # day
+    $args{day} = $1;
+    $args{month} = $2;
+    $args{year} = '20'.$3;
+
     return bless { %args }, $class;
 }
 
